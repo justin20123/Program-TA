01c274aec1cee253f216804fb414e12810ee19d3
justin20123 <justinandrew2001@gmail.com>
2024-11-06 18:53:16 +0700

    Update RatingController.php

--- tugasakhir-pemesan/app/Http/Controllers/RatingController.php
+++ tugasakhir-pemesan/app/Http/Controllers/RatingController.php
@@ -31,28 +31,28 @@ class RatingController extends Controller
             ->groupBy('notas.id')
             ->get();
             if($ratings->isNotEmpty()){
-               
                 $totalRating = 0;
                 $totalNota = DB::table('notas')
                 ->join('pemesanans', 'pemesanans.notas_id', '=', 'notas.id')
-                ->where('pemesanans.vendors_id', $idvendor)
-                ->count();
-                
+                ->where('pemesanans.vendors_id', '=', $idvendor)
+                ->distinct('notas.id') // Ensure distinct count
+                ->count('notas.id');
+        
                 foreach ($ratings as $r) {
                     $totalRating += $r->average_rating;
-                }   
-                     
-                $vendor_rating = $totalRating / ($totalNota/3);
+
+                }        
+                $vendor_rating = $totalRating / $totalNota;
                 return [
                     'vendor_rating' => $vendor_rating,
                     'total_nota' => $totalNota
-                ];           
+                ];              
             }
             else{
                 return [
                     'vendor_rating' => 3, //data netral u/ memberi peluang vendor baru
                     'total_nota' => 0
-                ]; 
+                ];
             }
         }
         else{
